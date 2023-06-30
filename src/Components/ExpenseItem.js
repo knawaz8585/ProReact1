5acf7acda0629a6080f2d87833b0bb49566a60f2
knawaz8585 <103634952+knawaz8585@users.noreply.github.com>
2023-06-30 21:13:30 +0530

    This is First React Project Upload

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ExpenseItem() {
+    return (
+        <div>
+            <h1>Expense Items</h1>
+            <p>Food Rs 10</p>
+            <p>Petrol Rs 100</p>
+            <p>Movies Rs 200</p>
+        </div>
+    )
+}
